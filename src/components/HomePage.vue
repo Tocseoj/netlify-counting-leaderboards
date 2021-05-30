389f365b2155040e3d9d1651622a780bbf9c9c02
Joe Scot Marshall <joe.scot.marshall@gmail.com>
2021-05-30 11:01:30 -0400

    minor ui

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,103 @@
 <script>
+import fitty from "fitty";
+
 export default {
   data() {
     return {
       foundUsers: [],
+      counters: [
+        {
+          snowflake: "142801087194333184",
+          username: "Tocseoj",
+          discriminator: "5982",
+          avatar: "dc5e2c0df21c6ab5e05df595781acce8",
+          fouls_hasty: "1",
+          fouls_badnumber: "2",
+          fouls_badcount: "8",
+          bonuses_mario: "0",
+          bonuses_runs: "0",
+          bonuses_sets: "0",
+          bonuses_primes: "0",
+        },
+        {
+          snowflake: "184132571389493248",
+          username: "Chance",
+          discriminator: "9820",
+          avatar: "c8080feb074317d809c7b190de909d2c",
+          fouls_hasty: "0",
+          fouls_badnumber: "1",
+          fouls_badcount: "0",
+          bonuses_mario: "0",
+          bonuses_runs: "0",
+          bonuses_sets: "0",
+          bonuses_primes: "0",
+        },
+        {
+          snowflake: "414298299487617026",
+          username: "pachiform",
+          discriminator: "3539",
+          avatar: "c81b7205e701df001becc13a3397b244",
+          fouls_hasty: "2",
+          fouls_badnumber: "0",
+          fouls_badcount: "2",
+          bonuses_mario: "0",
+          bonuses_runs: "0",
+          bonuses_sets: "0",
+          bonuses_primes: "0",
+        },
+        {
+          snowflake: "551886362337345543",
+          username: "Zesty Tony",
+          discriminator: "4391",
+          avatar: "642254c5804ecaa63cb5b61ee35a50bc",
+          fouls_hasty: "0",
+          fouls_badnumber: "0",
+          fouls_badcount: "5",
+          bonuses_mario: "0",
+          bonuses_runs: "0",
+          bonuses_sets: "0",
+          bonuses_primes: "0",
+        },
+        {
+          snowflake: "380334992074473474",
+          username: "mehehehe",
+          discriminator: "3717",
+          avatar: "c78f12b9e5e2c04737cfce4a7bfbc748",
+          fouls_hasty: "0",
+          fouls_badnumber: "0",
+          fouls_badcount: "2",
+          bonuses_mario: "0",
+          bonuses_runs: "0",
+          bonuses_sets: "0",
+          bonuses_primes: "0",
+        },
+        {
+          snowflake: "159805325736673280",
+          username: "顔のない",
+          discriminator: "4756",
+          avatar: "8faf3982f2cfadf9fba0beed6a8cc86b",
+          fouls_hasty: "0",
+          fouls_badnumber: "0",
+          fouls_badcount: "1",
+          bonuses_mario: "0",
+          bonuses_runs: "0",
+          bonuses_sets: "0",
+          bonuses_primes: "0",
+        },
+        {
+          snowflake: "321115167062491146",
+          username: "Vibrose",
+          discriminator: "0013",
+          avatar: "ba1100dc4aaecdde44ee51f31f2ce01d",
+          fouls_hasty: "0",
+          fouls_badnumber: "0",
+          fouls_badcount: "1",
+          bonuses_mario: "0",
+          bonuses_runs: "0",
+          bonuses_sets: "0",
+          bonuses_primes: "0",
+        },
+      ],
       counts: [
         { user: "Joe", count: 20 },
         { user: "Chance", count: 15 },
@@ -46,6 +141,8 @@ export default {
         { user: "Sctty28", count: 1 },
         { user: "Sctty29", count: 1 },
       ],
+      image_base_url: "https://cdn.discordapp.com/",
+      user_avatar_url: "avatars/${user_id}/${user_avatar}.png",
     };
   },
   mounted() {
@@ -54,6 +151,22 @@ export default {
     //   block: "start",
     //   inline: "start",
     // });
+    fitty(".fit", {
+      minSize: 12,
+      maxSize: 50,
+    });
+  },
+  methods: {
+    avatar(counter) {
+      const user = counter.snowflake;
+      const hash = counter.avatar;
+
+      let extension = ".png";
+      if (counter.avatar.substring(0, 2) === "a_") {
+        extension = ".gif";
+      }
+      return this.image_base_url + `avatars/${user}/${hash}` + extension;
+    },
   },
 };
 </script>
@@ -63,7 +176,21 @@ export default {
   <div class="case green">
     <h1 ref="top-of-page">Top Counters this week</h1>
     <div class="leaderboard-grid">
-      <div class="podium second ash-gray"></div>
+      <div class="user second">
+        <img
+          :src="avatar(counters[0])"
+          alt="discord avatar"
+          :style="{
+            height: '100px',
+            borderRadius: '100%',
+          }"
+        />
+      </div>
+      <div class="podium second ash-gray">
+        <div style="margin: 10px">
+          <div class="fit">{{ counters[0].username }}</div>
+        </div>
+      </div>
       <div class="podium first teal-blue"></div>
       <div class="podium third beige"></div>
     </div>
@@ -110,19 +237,27 @@ export default {
 .leaderboard-grid
   display: grid;
   grid-template-columns: 5% 30% 30% 30% 5%;
-  grid-template-rows: 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
   grid-template-areas:
-    ". .      first .     ."\
-    ". second first .     ."\
+    ". .      fuser .     ."\
+    ". suser  first .     ."\
+    ". second first tuser ."\
     ". second first third .";
 
 
-.first
+.first.podium
   grid-area: first;
-.second
+.second.podium
   grid-area second
-.third
+.third.podium
   grid-area third
+
+.first.user
+  grid-area: fuser;
+.second.user
+  grid-area suser
+.third.user
+  grid-area tuser
 
 .podium
   // margin-left 5px
